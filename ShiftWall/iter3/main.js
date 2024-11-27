cbf95bb76fe99fccc90b194ccc08adbdc275585e
Aditya Diwan <adidiwan123@hotmail.com>
2024-11-28 00:27:35 +0530

    minor bug fixes

--- ShiftWall/iter3/main.js
+++ ShiftWall/iter3/main.js
@@ -12,11 +12,12 @@ let slideIndex = 1;
 showSlides(slideIndex);
 
 function plusSlides(n) {
-    showSlides(slideIndex += n);
+  closeCredit()
+  showSlides(slideIndex += n);
 }
 
 function currentSlide(n) {
-    showSlides(slideIndex = n);
+  showSlides(slideIndex = n);
 }
 
 function showSlides(n) {
